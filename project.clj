c74e8909a4e045a460bf42f8c612d470e277fac7
Aaron Steele <eightysteele@gmail.com>
2012-06-10 14:36:31 -0700

    Add support for archive download, unzip, and open.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject dwca-reader-clj "0.1.0-SNAPSHOT"
   :description "Clojure wrapper for the GBIF Darwin Core Archive Reader library."
   :source-path "src/clj"
-  :repositories {"gbif" "http://repository.gbif.org/content/groups/gbif/"}
+  :repositories {"gbif" "http://repository.gbif.org/content/groups/gbif/"
+                 "zip4j" "http://mvnrepository.com/artifact/"}
   :jvm-opts ["-XX:MaxPermSize=128M"
              "-XX:+UseConcMarkSweepGC"
              "-Xms1024M" "-Xmx1048M" "-server"]
   :plugins [[lein-midje "1.0.8"]
             [swank-clojure "1.4.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.gbif/dwca-reader "1.9.1-SNAPSHOT"]])
+                 [org.gbif/dwca-reader "1.9.1-SNAPSHOT"]
+                 [net.lingala.zip4j/zip4j "1.3.1"]])
--- src/clj/dwca/core.clj
+++ src/clj/dwca/core.clj
@@ -1,1 +1,28 @@
-(ns dwca.core)
+(ns dwca.core
+  "This namespace provides a Clojure API to the GBIF dwca-reader library."
+  (:require [clojure.java.io :as io])
+  (:import [org.gbif.file DownloadUtil]
+           [org.gbif.dwc.record DarwinCoreRecord]
+           [org.gbif.dwc.text ArchiveFactory]
+           [java.io File]
+           [java.net URL]
+           [net.lingala.zip4j.core ZipFile]
+           [net.lingala.zip4j.exception ZipException]))
+
+(defn download
+  "Downloads a Darwin Core Archive from the supplied URL to the supplied dir."
+  [url dir]
+  (DownloadUtil/download (URL. url) (File. dir)))
+
+(defn unzip
+  "Unzips the supplied ZIP file into the supplied directory."
+  [file dir]
+  (let [zipfile (ZipFile. file)]
+    (.extractAll zipfile dir)))
+
+(defn get-records
+  "Returns a sequence of DarwinCoreRecord objects from an archive directory."
+  [dir]
+  (let [ archive (ArchiveFactory/openArchive (File. dir))]
+    (iterator-seq (.iteratorDwc archive))))
+
