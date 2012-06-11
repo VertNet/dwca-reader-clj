fa6cdb90cac2bafab166f5433d41da478d34b17b
Aaron Steele <eightysteele@gmail.com>
2012-06-10 21:42:18 -0700

    Update project config.

--- project.clj
+++ project.clj
@@ -3,13 +3,15 @@
   :source-path "src/clj"
   :resources-path "resources"
   :dev-resources-path "dev"
-  :repositories {"gbif" "http://repository.gbif.org/content/groups/gbif/"
+  :repositories {"conjars" "http://conjars.org/repo/"
+                 "gbif" "http://repository.gbif.org/content/groups/gbif/"
                  "zip4j" "http://mvnrepository.com/artifact/"
                  "maven2" "http://repo2.maven.org/maven2"}
   :jvm-opts ["-XX:MaxPermSize=128M"
              "-XX:+UseConcMarkSweepGC"
              "-Xms1024M" "-Xmx1048M" "-server"]
-  :plugins [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :plugins [[swank-clojure "1.4.0-SNAPSHOT"]
+            [lein-clojars "0.9.0"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.gbif/dwca-reader "1.9.1-SNAPSHOT"]
                  [net.lingala.zip4j/zip4j "1.3.1"]
