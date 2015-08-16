86cfbf325b1a36bbbd5608de279a469091f1ba58
tucotuco <tuco@berkeley.edu>
2015-08-16 15:23:18 +0200

    Forbidden to deploy to dwca-reader-clj. Not a group member, apparently. Trying new repo.

--- project.clj
+++ project.clj
@@ -1,10 +1,16 @@
-(defproject dwca-reader-clj/dwca-reader-clj "0.10.2-SNAPSHOT" 
+(defproject dwca-reader2-clj/dwca-reader-clj "0.20-SNAPSHOT" 
+  :description "Clojure wrapper for the GBIF Darwin Core Archive Reader library version 1.20."
+  :url "https://github.com/VertNet/dwca-reader-clj"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/license/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.gbif/dwca-reader "1.20"]
                  [net.lingala.zip4j/zip4j "1.3.1"]
                  [com.google.guava/guava "12.0"]
                  [clj-http "0.5.7"]
                  [org.gbif/gbif-common "0.16"]]
+  :scm {:name "git"
+        :url "https://github.com/VertNet/dwca-reader-clj"}
   :source-paths ["src/clj"]
   :profiles {:dev
              {:resource-paths
@@ -24,4 +30,6 @@
              "-Xms1024M"
              "-Xmx1048M"
              "-server"]
-  :description "Clojure wrapper for the GBIF Darwin Core Archive Reader library 1.20.")
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :signing {:gpg-key "gtuco.btuco@gmail.com"}
+)
